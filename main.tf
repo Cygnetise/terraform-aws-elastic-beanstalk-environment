5c821b0c5f97f97022a41b5db91141b84fa85fad
David Dawson <david.dawson@gmail.com>
2022-02-07 20:09:44 +0000

    restrict_public_buckets = true

--- main.tf
+++ main.tf
@@ -862,4 +862,5 @@ resource "aws_s3_bucket_public_access_block" "backups" {
   block_public_acls   = true
   block_public_policy = true
   ignore_public_acls  = true
+  restrict_public_buckets = true
 }
