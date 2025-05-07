44ecfd625073c45e565f8eb29aadd822fed27660
FLemon <jinfengxie1986@gmail.com>
2025-05-07 09:19:04 +0100

    remove default sg ingress inline to avoid conflict with standalone rule settings

--- main.tf
+++ main.tf
@@ -166,13 +166,6 @@ resource "aws_security_group" "default" {
 
   vpc_id = var.vpc_id
 
-  ingress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = -1
-    security_groups = var.allowed_security_groups
-  }
-
   egress {
     from_port   = 0
     to_port     = 0
